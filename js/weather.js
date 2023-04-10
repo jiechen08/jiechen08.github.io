6e8835ad484fb9b205c52eef686ca2ee623cc388
jiechen08 <jiechen0824@163.com>
2023-04-10 22:19:56 +0800

    Site updated: 2023-04-10 22:19:55

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "FF9900",
+    "citySize": "16",
+    "aqiColor": "4A86E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "155",
+    "top": "20",
+    "key": "4b30ca23a4cf4d17a96ef0fff53824f5"
+  }
+}
